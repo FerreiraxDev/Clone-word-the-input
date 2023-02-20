3d8896b1d82c02b307c7516aeeecc6a568afddf5
FerreiraxDev <96085002+FerreiraxDev@users.noreply.github.com>
2023-02-19 22:57:47 -0300

    Add files via upload

---
+++ Typing text down/assets/js/main.js
@@ -0,0 +1,7 @@
+
+function myShow(){
+    const boxwd = document.querySelector('#box-words');
+    const btnprint = document.querySelector('#btn');
+    const inputtxt = document.querySelector('#input').value;
+    boxwd.textContent = inputtxt;
+}
